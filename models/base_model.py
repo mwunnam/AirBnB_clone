527c47aacec9149f4becd7dd358fa074ec6eadb6
mwunnam <mwunnam@gmail.com>
2024-02-12 05:33:43 +0000

    I have added the args and kwargs in the init parameters and done the needfull to check all that is need to be checked

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     methods all other classes will inherited
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         This initializes a new instances of the BaseModel class.
 
@@ -21,9 +21,23 @@ class BaseModel:
             updated_at: Timestamp indicating when a class instance was up dated
             All these arttributes are public attribbutes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if "created_at" in kwargs:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                if "updated_at" in kwargs:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
